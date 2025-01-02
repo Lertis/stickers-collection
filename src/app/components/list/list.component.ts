e2657d5136b0e1a1da03df213daa298b860e1f3b
Yura Husak <yuriy.husak@emilfreydigital.hr>
2025-01-02 18:58:04 +0200

    add cover image and fleer #3 collection screenshoots

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -9,18 +9,18 @@ import { RoutePath } from '../../const'
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent {
-  readonly collections: { name: string, path: RoutePath }[] = [
-    { name: 'Dragon Ball Z (yellow dot)', path: RoutePath.DRAGON_BALL_Z_YELLOW_DOT },
-    { name: 'Dragon Ball Z (red dot)', path: RoutePath.DRAGON_BALL_Z_RED_DOT },
-    { name: 'Dragon Ball Z (black dot)', path: RoutePath.DRAGON_BALL_BLACK_DOT },
-    { name: 'Dragon Ball Z (no circle)', path: RoutePath.DRAGON_BALL_NO_CIRCLE },
-    { name: 'Dragon Ball fleer (#1)', path: RoutePath.DRAGON_BALL_FLEER_1 },
-    { name: 'Dragon Ball fleer (#2)', path: RoutePath.DRAGON_BALL_FLEER_2 },
-    { name: 'Dragon Ball fleer (#3)', path: RoutePath.DRAGON_BALL_FLEER_2 },
-    { name: 'Lion King', path: RoutePath.LION_KING },
-    { name: 'Digimons', path: RoutePath.DIGIMONS },
-    { name: 'Digimons shiny', path: RoutePath.DIGIMONS_SHINY },
-    { name: 'Jackie Chan', path: RoutePath.JACKIE_CHAN }
+  readonly collections: { name: string, path: RoutePath, cover: string, vertical: boolean }[] = [
+    { name: 'Dragon Ball Z (yellow dot)', path: RoutePath.DRAGON_BALL_Z_YELLOW_DOT, cover: `./assets/img/${RoutePath.DRAGON_BALL_Z_YELLOW_DOT}/cover.jpg`, vertical: true },
+    { name: 'Dragon Ball Z (red dot)', path: RoutePath.DRAGON_BALL_Z_RED_DOT, cover: `./assets/img/${RoutePath.DRAGON_BALL_Z_RED_DOT}/cover.jpg`, vertical: true },
+    { name: 'Dragon Ball Z (black dot)', path: RoutePath.DRAGON_BALL_BLACK_DOT, cover: `./assets/img/${RoutePath.DRAGON_BALL_BLACK_DOT}/cover.jpg`, vertical: true },
+    { name: 'Dragon Ball Z (no circle)', path: RoutePath.DRAGON_BALL_NO_CIRCLE, cover: `./assets/img/${RoutePath.DRAGON_BALL_NO_CIRCLE}/cover.jpg`, vertical: true },
+    { name: 'Dragon Ball fleer (#1)', path: RoutePath.DRAGON_BALL_FLEER_1, cover: `./assets/img/${RoutePath.DRAGON_BALL_FLEER_1}/cover.jpg`, vertical: true },
+    { name: 'Dragon Ball fleer (#2)', path: RoutePath.DRAGON_BALL_FLEER_2, cover: `./assets/img/${RoutePath.DRAGON_BALL_FLEER_2}/cover.jpg`, vertical: true },
+    { name: 'Dragon Ball fleer (#3)', path: RoutePath.DRAGON_BALL_FLEER_3, cover: `./assets/img/${RoutePath.DRAGON_BALL_FLEER_3}/cover.jpg`, vertical: true },
+    { name: 'Lion King', path: RoutePath.LION_KING, cover: `./assets/img/${RoutePath.LION_KING}/cover.jpg`, vertical: false },
+    { name: 'Digimons', path: RoutePath.DIGIMONS, cover: `./assets/img/${RoutePath.DIGIMONS}/cover.jpg`, vertical: true },
+    { name: 'Digimons shiny', path: RoutePath.DIGIMONS_SHINY, cover: `./assets/img/${RoutePath.DIGIMONS_SHINY}/cover.jpg`, vertical: true },
+    { name: 'Jackie Chan', path: RoutePath.JACKIE_CHAN, cover: `./assets/img/${RoutePath.JACKIE_CHAN}/cover.jpg`, vertical: false }
   ]
 
   constructor (private readonly router: Router) { }
