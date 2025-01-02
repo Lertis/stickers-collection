2f86cf652195e4275008b1b37d4ac170f0d41a39
Yura Husak <yuriy.husak@emilfreydigital.hr>
2025-01-02 19:38:03 +0200

    remove console log

--- src/app/root.ts
+++ src/app/root.ts
@@ -1,13 +1,8 @@
 import { Component } from '@angular/core'
-import { env } from './env/dev'
 
 @Component({
   selector: 'stk-root',
   templateUrl: './root.html',
   styleUrls: ['./root.scss']
 })
-export class RootComponent {
-  constructor () {
-    console.log(env.production)
-  }
-}
+export class RootComponent { }
